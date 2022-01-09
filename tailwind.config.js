5d4b6d28ccfbe359c8e368ac66c975af07b3086a
Qaudri <aquadirmuhammad@gmail.com>
2022-01-09 13:17:06 +0100

    fix: hotfix tailwindcss cli file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,68 @@
 module.exports = {
-  content: [],
+  purge:  {
+    enabled: false,
+    content: [
+      './pages/**/*.vue',
+      './**/*.html',
+      './*.html',
+    ],
+  },
+  
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      height: theme => ({
+        'screen-40': '40vh',
+        'screen-50': '50vh',
+        'screen-60': '60vh',
+        'screen-70': '70vh',
+        'screen-75': '75vh',
+        'screen-80': '80vh',
+        'screen-90': '90vh',
 
+      }),
+      
       colors: {
-        cyan:{
+
+        
+        cyan: {
+          '50': '#ecfeff',
           '100': '#cffafe',
           '200': '#a5f3fc',
           '300': '#67e8f9',
-          '400': '#00bdff',
+          '400': '#00BDFF',
           '500': '#06b6d4',
           '600': '#0891b2',
-          '700': '#0e7490',
-          '800': '#155e75'
         }
-      }
+        
+      },
+
+      zIndex: {
+        '10': 10,
+        '20': 20,
+        '30': 30,
+        '40': 40,
+        '50': 50,
+        '60': 60,
+        '70': 70,
+        '80': 80,
+        '90': 90,
+        '100': 100,
+        '110': 110,
+        '120': 120,
+        '130': 130,
+        '140': 140,
+        '150': 150,
+        'auto': 'auto',
+      },
+
+      display: [
+        "group-hover",
+      ]
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
